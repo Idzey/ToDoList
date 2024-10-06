bf2060c4379fc16af59ad891791bef3fb093d081
Idzey <kir.pan.787@gmail.com>
2024-10-07 02:04:47 +0300

    Date?

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,22 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Form({list, setList}) {
     const [name, setName] = useState("");
+    const [check, setCheck] = useState(false);
+    let checkRef = useRef();
     let inputRef = useRef();
+
+    useEffect(() => {
+        if (check) {
+            inputRef.current.type = "date";
+        } else {
+            inputRef.current.type = "text";
+        }
+    }, [check]);
+    function checkClick() {
+        setCheck(!check);
+    }
+
     function downEnter(evt) {
         if (evt.code == "Enter") createTask(evt);
     }
@@ -20,7 +34,12 @@ function Form({list, setList}) {
         <div className='h-1/4 flex flex-col justify-between rounded-sm'>
             <h2 className='border-b-2 text-3xl'>Add menu</h2>
             <div className='flex gap-10 h-full justify-center items-center p-5'>
-                <input ref={inputRef} onKeyDown={downEnter} onChange={evt => setName(evt.target.value)} type="text" name="" id="" placeholder='Name task...' className='p-3 bg-zinc-700' />
+                <input ref={inputRef} onKeyDown={downEnter} onChange={evt => setName(evt.target.value)} type="text" name="" id="" placeholder='Name task...' className='p-3 bg-zinc-700 w-70' />
+                <label className={"inline-flex items-center cursor-pointer"}>
+                    <input ref={checkRef} onClick={checkClick} type="checkbox" name="" id=""  className="sr-only peer"/>
+                    <span className={"ms-3 text-sm font-medium text-gray-900 dark:text-gray-300 mr-2"}>Date?</span>
+                    <div className={"relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"}></div>
+                </label>
                 <button className='py-3 px-6 bg-zinc-700' onClick={createTask}>add</button>
             </div>
         </div>
--- src/List.jsx
+++ src/List.jsx
@@ -11,7 +11,7 @@ function List({list, setList}) {
             <div className='h-full flex justify-center items-center'>
                 <ul className='h-full w-full max-h-full flex flex-col flex-wrap gap-4 py-4 px-10 content-start'>
                     {list.map((item, index) => 
-                        (<li className='flex justify-between items-center w-1/6 text-start border-b-2 py-2' key={index}>{index + 1}. {item} 
+                        (<li className='flex justify-between items-center w-1/3 text-start border-b-2 py-2' key={index}>{index + 1}. {item} 
                         <span onClick={deleteList} className="ml-2 text-red-600 cursor-pointer py-1 px-2 border rounded-md" id={index}>x</span>
                         </li>)
                     )}
